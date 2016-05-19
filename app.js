a49e1ef8b396be9008c160baff339042d88db10b
dipsikha <dipsikha21@gmail.com>
2016-05-19 15:11:10 -0400

    Search with autocomplete

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', routes);
 app.use('/users', users);
 
