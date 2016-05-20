db0b74c8c62c0a886a85f1dd2e3e982da9cb7234
dipsikha <dipsikha21@gmail.com>
2016-05-19 23:45:51 -0400

    Added charts.js

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,9 @@ router.get('/', function(req, res, next) {
 	var db = require('../db.js')
 	db.get().query('SELECT name, symbol FROM companies', function(err, results) {
     console.log(results)
-<<<<<<< HEAD
-    res.render('index', { title: 'Express', results: results });
-  });
-
-=======
     res.render('index', { title: 'Express', names: results});
   });
   
->>>>>>> 740972a81f50618d44d74c6af84672161a061d51
 });
 
 module.exports = router;
